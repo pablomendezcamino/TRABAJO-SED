c1ff645621b4280cb7d867fa5d851339d1cd0f05
Franciscomora <47852986+Franciscomora@users.noreply.github.com>
2021-12-17 20:32:35 +0100

    he hecho el debouncer
    he añadido los archivos debouncer  y debouncer_tb

--- sed_cronometro/sed_cronometro.srcs/sources_1/new/debouncer.vhd
+++ sed_cronometro/sed_cronometro.srcs/sources_1/new/debouncer.vhd
@@ -1,56 +1,57 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 16.12.2021 20:49:15
--- Design Name: 
--- Module Name: debuoncer_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity debouncer is
-    Port (
-        DATA: in std_logic;
+    port(
         CLK : in std_logic;
-        OP_DATA : out std_logic
+        RST_N : in std_logic;
+        button_in : in std_logic;
+        pulse_out : out std_logic
     );
 end debouncer;
 
-architecture Behavioral of debouncer is
-    Signal OP1, OP2, OP3: std_logic;
+architecture behavioral of debouncer is
+
+    --the below constants decide the working parameters.
+    --the higher this is, the more longer time the user has to press the button.
+    constant COUNT_MAX : integer := 60;
+    --set it '1' if the button creates a high pulse when its pressed, otherwise '0'.
+    constant BTN_ACTIVE : std_logic := '1';
+
+    signal count : integer := 0;
+    type state_type is (idle,wait_time); --state machine
+    signal state : state_type := idle;
+
 begin
-    Process(CLK)
+
+    process(RST_N,CLK)
     begin
-        If rising_edge(CLK) then
-            OP1 <= DATA;
-            OP2 <= OP1;
-            OP3 <= OP2;
+        if(RST_N = '1') then
+            state <= idle;
+            pulse_out <= '0';
+        elsif(rising_edge(CLK)) then
+            case (state) is
+                when idle =>
+                    if(button_in = BTN_ACTIVE) then
+                        state <= wait_time;
+                    else
+                        state <= idle; -- wait until button is pressed.
+                    end if;
+                    pulse_out <= '0';
+                when wait_time =>
+                    if(count = COUNT_MAX) then
+                        count <= 0;
+                        if(button_in = BTN_ACTIVE) then
+                            pulse_out <= '1';
+                        end if;
+                        state <= idle;
+                    else
+                        count <= count + 1;
+                    end if;
+            end case;
         end if;
     end process;
-    OP_DATA <= OP1 and OP2 and OP3;
-end architecture Behavioral;
+
+end architecture behavioral;
