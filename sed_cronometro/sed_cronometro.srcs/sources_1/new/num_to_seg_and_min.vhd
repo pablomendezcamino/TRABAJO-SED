947cf6cd59374880efe3a73e731546b8bc3df581
Franciscomora <47852986+Franciscomora@users.noreply.github.com>
2021-12-19 20:40:50 +0100

    he cambiado el divisor para que ponga un en vez de minutos y segundos solo un numero por en vez de 2
    En vez de dividirlo en minutos y segundos ahora lo divide en un numero por cuadrado, o sea, que solo hay un numero por division.

--- sed_cronometro/sed_cronometro.srcs/sources_1/new/num_to_seg_and_min.vhd
+++ sed_cronometro/sed_cronometro.srcs/sources_1/new/num_to_seg_and_min.vhd
@@ -1,53 +1,33 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 18.12.2021 19:55:06
--- Design Name: 
--- Module Name: num_to_seg_and_min - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 library IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 entity num_to_seg_and_min is
-port(
-    X   : in STD_LOGIC_VECTOR(15 downto 0);
-    Y   : in STD_LOGIC_VECTOR(15 downto 0);
-    R   : out STD_LOGIC_VECTOR(15 downto 0);
-    S   : out STD_LOGIC_VECTOR(15 downto 0)
-);
+    port(
+        NUM  : in STD_LOGIC_VECTOR(12 downto 0);
+        ones_sec : out STD_LOGIC_VECTOR(12 downto 0);
+        tens_sec : out STD_LOGIC_VECTOR(12 downto 0);
+        ones_min : out STD_LOGIC_VECTOR(12 downto 0);
+        tens_min : out STD_LOGIC_VECTOR(12 downto 0)
+    );
 end num_to_seg_and_min;
 
 architecture Behavioral of num_to_seg_and_min is
-signal SEC_i: std_logic_vector(X'range);
+    constant div : std_logic_vector(12 downto 0) := "0000000111100";
+    constant dec : std_logic_vector(12 downto 0) := "0000000001010";
+    signal SEC  : STD_LOGIC_VECTOR(12 downto 0);
+    signal MIN  : STD_LOGIC_VECTOR(12 downto 0);
+    signal tens_sec_i : std_logic_vector(tens_sec'range);
+    signal tens_min_i : std_logic_vector(tens_min'range);
 begin
-SEC_i <= std_logic_vector(to_signed(to_integer(signed(X) / signed(Y)),16));
-S <= std_logic_vector(to_signed(to_integer(signed(X)- signed(Y) * signed(SEC_i)),16));
-R <= SEC_i;
+
+    MIN <= std_logic_vector(to_signed(to_integer(signed(NUM)/signed(div)),13));
+    SEC <= std_logic_vector(to_signed(to_integer(signed(NUM)-signed(div)*signed(MIN)),13));
+    tens_sec_i <= std_logic_vector(to_signed(to_integer(signed(SEC)/signed(dec)),13));
+    ones_sec <= std_logic_vector(to_signed(to_integer(signed(SEC)-signed(dec)*signed(tens_sec_i)),13));
+    tens_sec <= tens_sec_i;
+    tens_min_i <= std_logic_vector(to_signed(to_integer(signed(MIN)/signed(dec)),13));
+    ones_min <= std_logic_vector(to_signed(to_integer(signed(MIN)-signed(dec)*signed(tens_min_i)),13));
+    tens_min <= tens_min_i;
 end Behavioral;
